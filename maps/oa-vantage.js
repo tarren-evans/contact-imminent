3066c0e2fdefb28194a050fcb30dfb730d47f4a5
Tarren Evans <tarren.evans@gmail.com>
2026-09-20 21:15:15 -0600

    v0.7.1.3 Campaign 02 runtime integration

---
+++ maps/oa-vantage.js
@@ -0,0 +1,14 @@
+window.CP_VANTAGE_MAP={
+ id:'OA-VANTAGE',name:'VANTAGE',region:'METRO-INDUSTRIAL BORDER CORRIDOR',seed:'VNT-02',
+ tactical:{width:900,height:650},
+ blufor:[
+  {id:'BLU-C2',x:150,y:285},{id:'BLU-AIR',x:735,y:135},{id:'BLU-LOG',x:755,y:345},
+  {id:'BLU-BORDER',x:360,y:490},{id:'BLU-PORT',x:620,y:585}
+ ],
+ hotspots:[
+  {id:'METRO-CORE',x:470,y:220},{id:'CENTRAL-RAIL',x:485,y:315},{id:'FREIGHT-YARD',x:470,y:390},
+  {id:'WAREHOUSE-DISTRICT',x:650,y:350},{id:'BORDER-XING-A',x:360,y:475},{id:'BORDER-XING-B',x:590,y:470},
+  {id:'PORT-TERMINAL',x:610,y:565},{id:'AIR-CARGO',x:745,y:155},{id:'SUBURBAN-TRANSIT',x:500,y:115},
+  {id:'HIGHWAY-SERVICE',x:760,y:260}
+ ]
+};
