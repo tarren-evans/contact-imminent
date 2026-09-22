bdb942901d09744f6c9486f812e7591ea33e6427
Tarren Evans <tarren.evans@gmail.com>
2026-09-22 17:36:20 -0600

    v0.9.1 Operational Area Expansion

---
+++ maps/oa-mirage.js
@@ -0,0 +1,5 @@
+window.CP_MIRAGE_MAP={
+ id:'OA-MIRAGE',name:'MIRAGE',region:'ARID ROUTE-LOGISTICS CORRIDOR',seed:'MRG-04',
+ tactical:{width:900,height:650},
+ status:'PREVIEW FOUNDATION'
+};
---
+++ maps/oa-tempest.js
@@ -0,0 +1,5 @@
+window.CP_TEMPEST_MAP={
+ id:'OA-TEMPEST',name:'TEMPEST',region:'MARITIME ARCHIPELAGO',seed:'TMP-05',
+ tactical:{width:900,height:650},
+ status:'PREVIEW FOUNDATION'
+};
